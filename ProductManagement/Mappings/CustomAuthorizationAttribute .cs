7cd65dd9e1ef09b062a291c79ecce8ed82d240b4
refatulfahad <refatm452@gmail.com>
2025-04-26 23:14:08 +0600

    feat: add custom authorization attribute

---
+++ ProductManagement/Mappings/CustomAuthorizationAttribute .cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace ProductManagement.Mappings
+{
+    public class CustomAuthorizationAttribute : Attribute, IAuthorizationFilter
+    {
+        public string Roles { get; set; }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.User;
+
+            if (!user.Identity?.IsAuthenticated ?? false)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(Roles))
+            {
+                var allowedRoles = Roles.Split(',');
+
+                var hasRole = allowedRoles.Any(role =>
+                    user.Claims.Any(c =>
+                    {
+                        if (string.IsNullOrWhiteSpace(c.Value))
+                            return false;
+
+                        var userRoles = c.Value.Split(',');
+                        return (c.Type == ClaimTypes.Role || c.Type == "roles" || c.Type == "role") &&
+                               userRoles.Any(x => string.Equals(x.Trim(), role.Trim(), StringComparison.OrdinalIgnoreCase));
+                    })
+                );
+
+
+                if (!hasRole)
+                {
+                    context.Result = new ForbidResult();
+                    return;
+                }
+            }
+        }
+    }
+}
