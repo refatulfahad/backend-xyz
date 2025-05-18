bfad9456fc9778502721e7ae8b3e394d4bbe6bfc
refatulfahad <refatm452@gmail.com>
2025-05-18 07:50:16 +0600

    feat: implement server side login

---
+++ ProductManagement/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using ProductManagement.Services;
+
+namespace ProductManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly KeycloakAuthService _keycloakAuthService;
+        public AuthController(KeycloakAuthService keycloakAuthService)
+        {
+            _keycloakAuthService = keycloakAuthService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            try
+            {
+                var token = await _keycloakAuthService.LoginAsync(request.Username, request.Password);
+                return Ok(token);
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+        }
+    }
+
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
