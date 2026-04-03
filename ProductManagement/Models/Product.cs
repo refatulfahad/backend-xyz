5245996d8148e5d21af99a64506ed7d84ccccf40
Md-Refatul-Fahad <refatul.fahad@brainstation-23.com>
2026-04-03 12:24:53 +0600

    fix: add imageurl

--- ProductManagement/Models/Product.cs
+++ ProductManagement/Models/Product.cs
@@ -3,9 +3,10 @@
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
--- ProductManagement/ViewModels/ProductDto.cs
+++ ProductManagement/ViewModels/ProductDto.cs
@@ -5,17 +5,19 @@ namespace ProductManagement.Domain
     public class ProductDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
+        public string? ImageUrl { get; set; }
     }
 
     public class UpsertProductDto
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
